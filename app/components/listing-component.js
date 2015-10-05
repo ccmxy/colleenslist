2e38450661fa4ac816006f43f18d8b32cd1d5bf0
ccmxy <windrainicefire@gmail.com>
2015-10-05 16:16:17 -0700

    added details show functionality

--- app/components/listing-component.js
+++ app/components/listing-component.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  areDetailsShowing: false,
   actions: {
+    detailsShow: function(){
+      this.set('areDetailsShowing', true);
+    },
+    detailseHide: function() {
+      this.set('areDetailsShowing', false);
+    },
   delete(listing) {
       if (confirm('Are you sure you want to delete this listing?')) {
         this.sendAction('destroyListing', listing);
