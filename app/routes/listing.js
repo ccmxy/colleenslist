86b17c21f7a6036103c06de85bfb705ded8da9ec
ccmxy <windrainicefire@gmail.com>
2015-10-05 16:27:47 -0700

    added listing page

---
+++ app/routes/listing.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  //apparently this function doesn't need to be called "model"
+  // but still must be referred to that way 
+  listing(params) {
+    return this.store.findRecord('listing', params.listing_id);
+  }
+});
