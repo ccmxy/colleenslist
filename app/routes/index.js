6635a40c57b574939c575041a01dec61fbdd072a
Colleen <windrainicefire@gmail.com>
2015-10-05 12:43:46 -0700

    added delete category functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
    return this.store.findAll('category');
- }
+ },
+ actions: {
+    destroyCategory(category) {
+      category.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
