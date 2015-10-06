522b665a7406b4fd5fe181ca4e3ea27a09a1beb6
ccmxy <windrainicefire@gmail.com>
2015-10-05 18:25:37 -0700

    listing images showing, improved css

--- app/routes/listing.js
+++ app/routes/listing.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   //apparently this function doesn't need to be called "model"
-  // but still must be referred to that way 
+  // but still must be referred to that way
   listing(params) {
     return this.store.findRecord('listing', params.listing_id);
+  },
+  itHasImage: true,
+  actions: {
   }
 });
