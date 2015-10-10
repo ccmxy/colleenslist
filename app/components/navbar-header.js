b4306009afc991d28f8ef52d7405425b174d0469
ccmxy <windrainicefire@gmail.com>
2015-10-09 22:42:58 -0700

    improved navbar redirect

--- app/components/navbar-header.js
+++ app/components/navbar-header.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   model() {
      return this.store.findAll('category');
+   },
+   actions: {
+     categorySelected: function(){
+       debugger;
+       var e = document.getElementById("categories");
+       var catId = e.options[e.selectedIndex].value;
+       //I have verified that catId contains the appropriate ID at this point.
+
+       //Where the error happens:
+    //  this.transitionTo('/category/' + catId);
+      //this.transitionTo(('/:question_id', params.question);
+	     this.transitionTo({path:'/category/:category_id'}, catId);
+     }
    }
 });
